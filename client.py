ff0dc400176194fdc133b03d89ded198a367393c
spencerckhuang <shuan126@jh.edu>
2023-01-24 21:25:28 -0500

    fixed issue in printing correct values from revised methods:

--- client.py
+++ client.py
@@ -42,21 +42,22 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    if price_b == 0:
+    if (price_b == 0):
         return
     return price_a / price_b
-    return 1
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    prices = {'ABC': 0, 'DEF': 0} # initialize dictionary to hold two stocks to be compared
+
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            stock, bid_price, ask_price, prices[stock] = getDataPoint(quote) # update price of current stock
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, prices[stock]))
 
-        print("Ratio %s" % getRatio(price, price))
+        print("Ratio %s" % getRatio(prices['ABC'], prices['DEF']))
